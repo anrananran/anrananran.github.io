9d4e1328fb650a48a8e732283ae0cabe715d00b3
zengjian <536500204@qq.com>
2020-04-01 14:10:41 +0800

    add fireworks

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,17 @@
   <div class="home">
     <div class="home-items">
       <ul>
-        <li v-for="item in items" :key="item.link" class="home-item">
-          <a target="_blank" :href="item.link">
-            <h2 class="name">{{ item.name }}</h2>
-            <p class="desc">{{ item.desc }}</p>
-          </a>
+        <li v-for="item in items" :key="item.link" class="home-item" @click="onClickLink(item.link)">
+          <h2 class="name" :title="item.name">{{ item.name }}</h2>
+          <div class="bottom">
+            <div class="tags">
+              <span v-for="tag in item.tags" :key="tag" class="tag">{{ tag }}</span>
+            </div>
+            <div class="from">
+              <span class="author" :title="item.author.name" @click.stop="onClickLink(item.author.homePage)">作者：{{ item.author.name }}</span>
+              <span :title="item.from" @click.stop="onClickLink(item.from)">来源：{{ item.from }}</span>
+            </div>
+          </div>
         </li>
       </ul>
     </div>
@@ -22,6 +28,11 @@ export default {
     return {
       items
     }
+  },
+  methods: {
+    onClickLink(link) {
+      window.open(link, '_blank')
+    }
   }
 }
 </script>
@@ -34,27 +45,61 @@ export default {
   }
   .home-item {
     margin-bottom: 10px;
-  }
-  .home-item > a {
-    display: block;
     background-color: #2F333B;
     padding: 20px;
     border-radius: 5px;
     border: 1px solid #191A1E;
-  }
-  .home-item > a:hover {
-    background-color: #333741;
-  }
-  .home-item .name {
-    color: #D3D5DC;
-    font-size: 20px;
-    margin: 0 0 15px;
-  }
-  .home-item .desc {
-    color: #9298A7;
-    font-size: 16px;
-    margin: 0;
-    line-height: 1.8;
+
+    &:hover {
+      background-color: #333741;
+      cursor: pointer;
+    }
+
+    .name {
+      color: #D3D5DC;
+      font-size: 24px;
+      font-weight: 400;
+      margin: 0 0 15px;
+    }
+    .bottom {
+      line-height: 30px;
+
+      &:after {
+        content: "\20";
+        display: block;
+        clear: both;
+        overflow: hidden;
+        height: 0;
+      }
+    }
+    .tags {
+      color: #9298A7;
+      font-size: 16px;
+      margin: 0;
+      line-height: 1.8;
+      float: left;
+    }
+    .tag {
+      margin: 0 10px 0 0;
+      background-color: #5271C7;
+      padding: 5px 10px;
+      color: #fff;
+      font-size: 14px;
+      border: 1px solid #191A1E;
+      border-radius: 4px;
+    }
+    .from {
+      float: right;
+      color: #9298A7;
+      cursor: pointer;
+      width: 500px;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+    }
+    .author {
+      margin-right: 10px;
+    }
   }
 }
 </style>
