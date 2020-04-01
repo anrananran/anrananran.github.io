03c0ff2a41d0edc0397eea1a9363367572d791cd
zengjian <536500204@qq.com>
2020-04-01 14:39:09 +0800

    edit style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,16 @@
     <div class="home-items">
       <ul>
         <li v-for="item in items" :key="item.link" class="home-item" @click="onClickLink(item.link)">
-          <h2 class="name" :title="item.name">{{ item.name }}</h2>
-          <div class="bottom">
+          <iframe class="iframe" width="240" height="150" :src="item.link" frameborder="0" scrolling="no" seamless></iframe>
+          <div class="right">
+            <h2 class="name" :title="item.name">{{ item.name }}</h2>
             <div class="tags">
               <span v-for="tag in item.tags" :key="tag" class="tag">{{ tag }}</span>
             </div>
             <div class="from">
               <span class="author" :title="item.author.name" @click.stop="onClickLink(item.author.homePage)">作者：{{ item.author.name }}</span>
+            </div>
+            <div class="from">
               <span :title="item.from" @click.stop="onClickLink(item.from)">来源：{{ item.from }}</span>
             </div>
           </div>
@@ -40,7 +43,7 @@ export default {
 <style lang="scss" scoped>
 .home {
   .home-items {
-    width: 800px;
+    width: 900px;
     margin: 0 auto;
   }
   .home-item {
@@ -49,35 +52,32 @@ export default {
     padding: 20px;
     border-radius: 5px;
     border: 1px solid #191A1E;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
 
     &:hover {
       background-color: #333741;
       cursor: pointer;
     }
-
+    .iframe {
+      margin-right: 20px;
+    }
+    .right {
+      flex: 1;
+    }
     .name {
       color: #D3D5DC;
       font-size: 24px;
       font-weight: 400;
-      margin: 0 0 15px;
+      margin: 6px 0 15px;
     }
-    .bottom {
-      line-height: 30px;
-
-      &:after {
-        content: "\20";
-        display: block;
-        clear: both;
-        overflow: hidden;
-        height: 0;
-      }
-    }
+    
     .tags {
       color: #9298A7;
       font-size: 16px;
-      margin: 0;
+      margin: 0 0 15px;
       line-height: 1.8;
-      float: left;
     }
     .tag {
       margin: 0 10px 0 0;
@@ -89,13 +89,14 @@ export default {
       border-radius: 4px;
     }
     .from {
-      float: right;
       color: #9298A7;
       cursor: pointer;
-      width: 500px;
+      line-height: 1.8;
+      width: 400px;
       text-overflow: ellipsis;
       overflow: hidden;
       white-space: nowrap;
+      margin-bottom: 5px;
     }
     .author {
       margin-right: 10px;
